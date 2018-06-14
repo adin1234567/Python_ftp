5478f1cbe7ae96641ceba6609bbbf943c9ec48d7
rejeffery9311 <40259541+rejeffery9311@users.noreply.github.com>
2018-06-14 01:41:32 -0700

    Add files via upload
    幹你的 自己寫好ㄇ孫大澤

--- Iot_ftp.py
+++ Iot_ftp.py
@@ -1,7 +1,39 @@
 from ftplib import FTP
 import datetime
-import schedule
+##import schedule
 import time
+class setUp:
+    def __init__(self):
+        self._ftp = ''
+        self._user =''
+        self._password =''
+        self._CSV_folder =''
+        self._CSV_name =''
+        self._date = ''
+    def setFTP(self, ftp):
+        self._ftp = ftp
+    def getFTP(self):
+        return self._ftp
+    def setUser(self, user):
+        self._user = user
+    def getUser(self):
+        return self._user
+    def setPassword(self, password):
+        self._password = password
+    def getPassword(self):
+        return self._password
+    def setCSV_folder(self, folder):
+        self._CSV_folder = folder
+    def getCSV_folder(self):
+        return self._CSV_folder
+    def setCSV_name(self, name):
+        self._CSV_name = name
+    def getCSV_name(self):
+        return self._CSV_name
+    def setDate(self):
+        self._date = datetime.datetime.now()
+    def getDate(self):
+        return str(self._date).replace(':','-').replace(' ','-').replace('.','-').split('-')[-1]
 
 def getFile(ftp, filename):
     try:
@@ -10,42 +42,29 @@ def getFile(ftp, filename):
         print("Error")
 
 
-def job():
-    now = datetime.datetime.now()
-    if now.month<10:
-        month='0'+str(now.month)
-    else:
-        month=str(now.month)
-
-    if now.day<10:
-        day='0'+str(now.day) 
-    else:
-        day=str(now.day)
-
-    if now.hour<10:
-        hour='0'+str(now.hour)
-    else:
-        hour=str(now.hour)
-  
-
-
-    ftp = FTP('192.72.189.223') 
-    ftp.login('admin','Admin')
-
-    Csv_folder='/LOG/Folder1/'+str(now.year)+month
-    File_name=month+day+'_'+hour
-
-    print('Will download:'+File_name)
-
-    ftp.cwd(Csv_folder) 
+def job(obj):
+    ftp = FTP(obj.getFTP())
+    ftp.login(obj.getUser(),obj.getPassword())
+    print('Will download:'+obj.getCSV_name())
+    ftp.cwd(obj.getCSV_folder())
     data = []
- 
     ftp.dir(data.append)
- 
- 
     for line in data:
         print("-", line)
-    getFile(ftp,File_name+'.csv')
+    getFile(ftp,obj.getCSV_name()+'.csv')
     ftp.quit()
     ftp.close()
-
+    
+def main():
+    '''set up here'''
+    obj = setUp()
+    obj.setFTP('192.72.189.223')
+    obj.setUser('admin')
+    obj.setPassword('Admin')
+    obj.setDate()
+    obj.setCSV_folder('/LOG/Folder1/'+obj.getDate()[0]+obj.getDate()[1])
+    obj.setCSV_name(obj.getDate()[1]+obj.getDate()[2]+'_'+obj.getDate()[3])
+    '''///////////'''
+    job(obj)
+if __name__ == "__main__":
+    main()
